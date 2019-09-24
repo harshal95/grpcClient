56367fad79d0519e540055d154db8aa4d959d339
harshal95 <psriharshal7@gmail.com>
2019-09-23 23:36:21 -0500

    fixed kill script of server

--- scripts/killprocesses.py
+++ scripts/killprocesses.py
@@ -3,17 +3,26 @@ import sys
 import os
 
 if __name__ == "__main__":
-    cmd = ["lsof", "-ti"]
-    killcmd = ["kill", "-9"]   
+    cmd_template = ["lsof", "-ti"]
+    killcmd_template = ["kill", "-9"]   
     n = sys.argv[1] # Number of processes
     base_port = sys.argv[2] # Base port
     for i in range(int(n)):
-        port = i + base_port
-        cmd.append("TCP:" + str(port))
-        portnum = subprocess.check_output(cmd)
-        print(type(portnum))
-        pn = portnum.decode('utf-8').rstrip('\n')
-        killcmd.append(str(pn))
-        subprocess.check_output(killcmd)
-        cmd = cmd [: -1]
-        killcmd = killcmd[: -1]
+        port = i + int(base_port)
+        cmd_list = cmd_template[:]
+        cmd_list.append("tcp:" + str(port))
+        print(cmd_list)
+        try:
+            pid_string = subprocess.check_output(cmd_list)
+            pid_list = pid_string.decode('utf-8').split('\n')[:-1]
+            print(pid_list)
+            for p in pid_list:
+                killcmd_list = killcmd_template[:]
+                killcmd_list.append(str(p))
+                try:
+                    subprocess.check_output(killcmd_list)
+                except subprocess.CalledProcessError as kill_exec:
+                    print("call to kill failed")
+        except subprocess.CalledProcessError as pid_exec:
+            print("call to lsof failed")
+   
